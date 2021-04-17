864a2c08a20520dd63784c8876cfc5e96dfaf8a9
fajrin200 <muhammadfajrin412@gmail.com>
2021-04-17 21:07:17 +0700

    Latihan Praktikum 9 Membuat relasi many to many

---
+++ app/Models/Matakuliah.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Mahasiswa;
+
+
+class Matakuliah extends Model
+{
+    use HasFactory;
+    protected $table = "Matakuliah";
+    
+    public function mahasiswa()
+    {
+        return $this->belongsToMany(Mahasiswa::class)->withpivot('nilai');
+    }
+
+   
+}
